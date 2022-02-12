b77289fafaccef90b5ca85cca9798ee950f0cd60
Ben Snow <the.ben.snow@gmail.com>
2022-02-12 15:06:12 -0500

    Fix typescript config.

---
+++ node-ts.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    '@bensnow/eslint-config/lib/base',
+    '@bensnow/eslint-config/lib/typescript',
+    '@bensnow/eslint-config/lib/node',
+  ],
+}
